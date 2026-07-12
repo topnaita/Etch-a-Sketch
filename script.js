6384de27d613fefd54f6daff4fd6351e4b5a8f1e
javier pena <123456789+odin@users.noreply.github.com>
2026-07-11 23:36:17 -0400

    Resizing squares is working now, still missing remove child after deleting the currents squares

--- script.js
+++ script.js
@@ -6,12 +6,20 @@ btnCreateDiv.addEventListener("click", () => {
 }); // Callback btn Create DIV
 
 function createDiv() {
-  // num = prompt("Insert the amount of squares");
+  let num = prompt("Insert the amount of squares");
+  let pixel = num * num;
+  let percentStyle = 100 / num;
 
-  for (let i = 0; i < 256; i++) {
+  for (let i = 0; i < `${pixel}`; i++) {
+    if (num > 100) {
+      alert("Please DO NOT enter a number greater than 100");
+      break;
+    } else {
+    }
+
     const divElement = document.createElement("div");
-    divElement.style.width = "6.25%";
-    divElement.style.height = "6.25%";
+    divElement.style.width = `${percentStyle}%`;
+    divElement.style.height = `${percentStyle}%`;
     divElement.style.border = "1px solid #ccc";
     divElement.style.borderColor = "black";
     divElement.style.boxSizing = "border-box";
