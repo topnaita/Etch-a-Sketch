ac1720e9edb6278d70145dc6a8f237027f412b60
javier pena <123456789+odin@users.noreply.github.com>
2026-07-12 22:44:12 -0400

    I add a reset button, and UI is completed.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const container = document.querySelector(".container");
 const btnCreateDiv = document.querySelector("#createDiv");
+const btnDeleteDiv = document.querySelector("#deleteDiv");
 
 btnCreateDiv.addEventListener("click", () => {
   createDiv();
 }); // Callback btn Create DIV
+
+btnDeleteDiv.addEventListener("click", () => {
+  container.replaceChildren();
+});
 
 //logic
 function createDiv() {
